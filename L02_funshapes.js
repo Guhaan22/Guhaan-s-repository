107af3621a494612c423168bde42af23686342e4
Guhaan22 <lakshmi.rbala@gmail.com>
2025-07-16 08:20:45 +0000

    Jul 16, 2025, 8:20 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -4,6 +4,7 @@ Task 1: Add the setup() function
 function setup() {
   createCanvas(600, 400);
   background(50);// 0 to 255 gray tone
+  noLoop(); // Stops continuous drawing
   
 }
 function draw() {

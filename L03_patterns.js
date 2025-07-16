f0ca4eb3edfeefe14bd8ea1bcb59d66cfcfaeca8
Guhaan22 <lakshmi.rbala@gmail.com>
2025-07-16 08:13:08 +0000

    Jul 16, 2025, 8:13 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -60,7 +60,20 @@ function draw() {
   fill(255, 0, 0);
   ellipse(250, 250, 30, 30); // Left cheek
   ellipse(350, 250, 30, 30); // Right cheek
-  
+  // Recap 9: A funny Pokemon using the functions you have learned
+  fill(255, 255, 0);
+  ellipse(300, 300, 150, 150); // Body
+  fill(0, 0, 0);
+  ellipse(270, 280, 30, 30); // Left eye
+  ellipse(330, 280, 30, 30); // Right eye
+  fill(255, 0, 0);
+  ellipse(300, 320, 50, 20); // Mouth
+  fill(0, 255, 0);
+  ellipse(300, 350, 100, 50); // Belly
+  fill(255, 0, 0);
+  ellipse(250, 250, 30, 30); // Left cheek
+  ellipse(350, 250, 30, 30); // Right cheek
+    
 
   // Task 1: Colour Gradient
 

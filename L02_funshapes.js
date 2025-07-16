a9809814ae6985aa007cecd17bd7871024b21d6d
Guhaan22 <lakshmi.rbala@gmail.com>
2025-07-16 07:53:29 +0000

    Jul 16, 2025, 7:53 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -27,5 +27,6 @@ function draw() {
 
   // Challenge 6: Add some colours to the drawings in the above challenges
 
-  // Challenge 7: Create a funny Pokemon
+  // Challenge 7: Create a funny Pokemon using the functions you have learned
+  fill(255, 255, 0);
 }

571d42b1b662cbb8af8735d2b54fdef65875cf76
Guhaan22 <lakshmi.rbala@gmail.com>
2025-07-16 08:20:36 +0000

    Jul 16, 2025, 8:20 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -4,12 +4,13 @@ Task 1: Add the setup() function
 function setup() {
   createCanvas(600, 400);
   background(50);// 0 to 255 gray tone
+  
 }
 function draw() {
   // Challenge 1: An eye using ellipse() function
   fill("hotpink");
   ellipse(300, 200, 100, 100);
-  
+
 
   fill(255, 0, 0);
   ellipse(300, 200, 200, 200);

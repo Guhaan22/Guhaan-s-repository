6e2527cb4000bd4007c083eda65fae9adec1dc0e
Guhaan22 <lakshmi.rbala@gmail.com>
2025-07-16 08:10:01 +0000

    Jul 16, 2025, 8:10 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -54,7 +54,18 @@ function draw() {
   ellipse(350, 250, 30, 30); // Right cheek
   // Challenge 7: Create a funny Pokemon using the functions you have learned
   fill(255, 255, 0);
-    
+  ellipse(300, 300, 150, 150); // Body
+  fill(0, 0, 0);
+  ellipse(270, 280, 30, 30); // Left eye
+  ellipse(330, 280, 30, 30); // Right eye
+  fill(255, 0, 0);
+  ellipse(300, 320, 50, 20); // Mouth
+  fill(0, 255, 0);
+  ellipse(300, 350, 100, 50); // Belly
+  fill(255, 0, 0);
+  ellipse(250, 250, 30, 30); // Left cheek
+  ellipse(350, 250, 30, 30); // Right cheek
+  
  
   // Challenge 8: Create a funny face using the functions you have learned
   fill(255, 220, 185);

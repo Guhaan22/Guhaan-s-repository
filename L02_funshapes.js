edc2c8ec133e3fd8260e6cd3841bddcb1ecdb2b4
Guhaan22 <lakshmi.rbala@gmail.com>
2025-07-16 07:52:14 +0000

    Jul 16, 2025, 7:52 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -3,7 +3,8 @@ Task 1: Add the setup() function
 */
 
 function draw() {
-  // Challenge 1: An eye using ellipse() function
+  // Challenge 1: An eye using ellipse() function\
+
 
   // Challenge 2: A face using circle() function
 

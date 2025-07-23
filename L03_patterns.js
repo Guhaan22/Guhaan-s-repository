552129c0f03ace6534ec28a034ce7de56e490c68
Guhaan22 <lakshmi.rbala@gmail.com>
2025-07-23 07:53:21 +0000

    Jul 23, 2025, 7:53 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -4,32 +4,8 @@ function setup() {
 
 }
 function draw() {
+ circle(75, 200, 200)
+ circle(225, 200, 200)
+ circle(375, 200, 200)
+ circle(75, 200, 200)
  
-  // Task 1: Colour Gradient
-  for (let i = 0; i < width; i += 10) {
-    let c = map(i, 0, width, 0, 255);
-    fill(c, 100, 150);
-    rect(i, height - 50, 10, 50);
-  }
-  
-
-  // Task 2: Colour Loop
-  for (let i = 0; i < 360; i += 10) {
-    stroke(i, 100, 150);
-    line(0, i, width, i);
-  }
-
-  // Task 3: Row of Circles
-  for (let i = 0; i < width; i += 50) {
-    fill(100, 150, 200);
-    ellipse(i + 25, height - 100, 50, 50);
-  }
-
-  // Task 4: Grid of Circles
-  for (let x = 0; x < width; x += 50) {
-    for (let y = 0; y < height - 100; y += 50) {
-      fill(100, 150, 200);
-      ellipse(x + 25, y + 25, 50, 50);
-    }
-  }
-}

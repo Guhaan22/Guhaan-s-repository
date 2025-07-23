46d98e8fa7a407fe28a74cf1ff369c7926816544
Guhaan22 <lakshmi.rbala@gmail.com>
2025-07-23 07:59:21 +0000

    Jul 23, 2025, 7:59 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -1,9 +1,8 @@
 function setup() {
   createCanvas(600, 400);
   background(220);// 0 to 255 gray tone
-  // noLoop();
-}
-function draw() {
+  // noLoop();  // stops continuous drawing //
+function draw() {}
  circle(75, 200, 150);
  circle(225, 200, 150);
  circle(375, 200, 150);

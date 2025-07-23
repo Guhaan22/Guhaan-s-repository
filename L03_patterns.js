b77ad61fbd1076fd45cc60bc1fde6c83b9c277df
Guhaan22 <lakshmi.rbala@gmail.com>
2025-07-23 08:26:42 +0000

    Jul 23, 2025, 8:26 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -15,7 +15,7 @@ let posx = 75;
   // recap 1 repeating circles using for loop
   for (let i = 0; i < 4; i++) {
     fill (0, i*50, 200);
-    circle(posx + i * 150, 200, diameter);
+    circle(posx + i * 15, 200, diameter);
     posx = posx + diameter + 10 // calculate  the next position of x
   }
 } 

9ef350b00336373447416d984700748695ea0344
Guhaan22 <lakshmi.rbala@gmail.com>
2025-07-23 07:54:37 +0000

    Jul 23, 2025, 7:54 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -4,8 +4,8 @@ function setup() {
 
 }
 function draw() {
- circle(75, 200, 150)
- circle(225, 200, 150)
- circle(375, 200, 150)
- circle(525, 200, 150)
+ circle(75, 200, 150);
+ circle(225, 200, 150);
+ circle(375, 200, 150);
+ circle(525, 200, 150);
 }

7edc68f6d0ea2ffb9c1037b2c726e32ba8d2dfeb
Guhaan22 <lakshmi.rbala@gmail.com>
2025-07-23 08:17:31 +0000

    Jul 23, 2025, 8:17 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -3,10 +3,10 @@ function setup() {}
   background(220);// 0 to 255 gray tone
 // noLoop();  // stops continuous drawing //]
 function draw() {
- circle(75, 200, 150);
- circle(225, 200, 150);
- circle(375, 200, 150);
- circle(525, 200, 150);
+//  circle(75, 200, 150);
+//  circle(225, 200, 150);
+//  circle(375, 200, 150);
+//  circle(525, 200, 150);
 
  let diameter = 80;
 let posx = 75;

f2b7be5e0da7b1e5144c89e3ba90ab37a910850c
Guhaan22 <lakshmi.rbala@gmail.com>
2025-09-03 07:42:34 +0000

    Sep 3, 2025, 7:42 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -1,1 +1,40 @@
+let xpos = 50
+let ypos = 50
+
+
+function setup() {
+  createCanvas(400, 400);
+}
+
 // write your codes here
+function draw() {
+  background(200); // clear the drawing paper
+// if(keyCode === 38){   fill(255, 0, 0);
+//     }
+// if (keyCode === 40){
+//     }
+
+textSize(18); // font Size
+   // text     x y
+text(key,    50, 70)
+text(keyCode, 50, 40)
+// ellipse(300 ,100 ,100 ,100 );
+
+if(keyIsDown( RIGHT_ARROW)){
+    xpos = xpos +3;
+  }
+  if(keyIsDown( LEFT_ARROW)){
+    xpos = xpos -3;
+  }
+  xpos = constrain(xpos, 0, width);
+  if(keyIsDown( UP_ARROW)){
+    ypos = ypos -3;
+  }
+  if(keyIsDown( DOWN_ARROW)){
+    ypos = ypos +3;
+  }
+  ypos = constrain(ypos, 0, height)
+
+  fill(255, 0, 0); // red
+  circle(xpos, ypos, 50); // width, height of the canvas
+}
